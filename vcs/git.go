dbabad0b3425e4200f6c92a36374f81e75483fb8
Ad van der Veer <advanderveer@gmail.com>
2015-05-15 22:28:21 +0200

    now correctly write commit template to commit hook

--- vcs/git.go
+++ vcs/git.go
@@ -67,7 +67,7 @@ func (g *Git) Hook() error {
 		return errwrap.Wrapf(fmt.Sprintf("Failed to make post-commit file '%s' executable: {{err}}", hpath), err)
 	}
 
-	err = PostCheckoutTmpl.Execute(postcof, struct{}{})
+	err = PostCommitTmpl.Execute(postcof, struct{}{})
 	if err != nil {
 		return errwrap.Wrapf("Failed to run post-commit template: {{err}}", err)
 	}
