455d4f58a2f0de5153bfecb063acedbdf6aec766
Ad van der Veer <advanderveer@gmail.com>
2015-05-24 16:26:40 +0200

    sum now accepts both stdin and arguments [spent 36m10s]

--- command/sum.go
+++ command/sum.go
@@ -53,27 +53,47 @@ func (c *Sum) Run(ctx *cli.Context) error {
 		return errwrap.Wrapf("Failed to setup VCS: {{err}}", err)
 	}
 
-	var total time.Duration
+	//retrieve commits through piped stdin or arguments
+	commits := []string{}
 	if !isatty.IsTerminal(os.Stdin.Fd()) {
 		scanner := bufio.NewScanner(os.Stdin)
 		for scanner.Scan() {
-			line := scanner.Text()
-
-			data, err := vc.Show(line)
-			if err != nil {
-				if err == vcs.ErrNoCommitTimeData {
-					//ignore if a commit has no time attached
-					continue
-				}
-
-				return errwrap.Wrapf(fmt.Sprintf("Failed to show time notes for '%s': {{err}}", line), err)
-			}
-
-			total += data.Total()
+			commits = append(commits, scanner.Text())
 		}
 		if err := scanner.Err(); err != nil {
 			fmt.Println(err)
 		}
+	} else {
+		if ctx.Args().First() != "" {
+			commits = append(commits, ctx.Args().First())
+		}
+		commits = append(commits, ctx.Args().Tail()...)
+	}
+
+	if len(commits) == 0 {
+		return fmt.Errorf("Please provide at least one commit through STDIN or as an argument.")
+	}
+
+	//map time data from the vcs
+	list := []vcs.TimeData{}
+	for _, c := range commits {
+		data, err := vc.Show(c)
+		if err != nil {
+			if err == vcs.ErrNoCommitTimeData {
+				//ignore if a commit has no time attached
+				continue
+			}
+
+			return errwrap.Wrapf(fmt.Sprintf("Failed to show time notes for '%s': {{err}}", c), err)
+		}
+
+		list = append(list, data)
+	}
+
+	//reduce to output
+	var total time.Duration
+	for _, data := range list {
+		total += data.Total()
 	}
 
 	fmt.Println(total)
