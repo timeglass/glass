3c70fd863c7f870b8e70c12e7d223d644f91745f
Ad van der Veer <advanderveer@gmail.com>
2015-06-09 18:49:33 +0200

    cross compiling to invovled on windows, for now use a VM setup with only amd64 as the target architecture [spent 18m40s]

--- make.bash
+++ make.bash
@@ -2,12 +2,6 @@
 GOOS=`go env GOOS`
 GOARCH=`go env GOARCH`
 XGOARCH=$GOARCH
-if [ "$XGOARCH" == "amd64" ]
-then
-	XGOARCH="386"
-else 
-	XGOARCH="amd64"
-fi
 
 function run_build_daemon {
 	go build -o $GOPATH/bin/glass-daemon -ldflags "-X main.Version `cat VERSION` -X main.Build `date -u +%Y%m%d%H%M%S`" ./glass-daemon
@@ -34,15 +28,6 @@ function run_build {
 	run_build_daemon
 }  
 
-function run_xbuild {
-	echo "Cross Compiling CLI for '$XGOARCH'..."
-	mkdir -p bin/${GOOS}_${XGOARCH}
-	CGO_ENABLED=1 GOARCH=$XGOARCH go build -o bin/${GOOS}_${XGOARCH}/glass -ldflags "-X main.Version `cat VERSION` -X main.Build `date -u +%Y%m%d%H%M%S`" .
-
-	echo "Cross Compiling Daemon for '$XGOARCH'..."
-	CGO_ENABLED=1 GOARCH=$XGOARCH go build -o bin/${GOOS}_${XGOARCH}/glass-daemon -ldflags "-X main.Version `cat VERSION` -X main.Build `date -u +%Y%m%d%H%M%S`" ./glass-daemon
-}
-
 function run_release_prepare_dirs {
 	echo "creating release directories..."
 	rm -fr bin/${GOOS}*
@@ -55,7 +40,6 @@ function run_release {
 	run_test
 	run_build
 	run_release_prepare_dirs
-	run_xbuild
 }  
 
 #choose command
@@ -65,7 +49,6 @@ case $1 in
     "test") run_test ;;
     "build" ) run_build ;;
 	"run-daemon" ) run_run_daemon ;;
-	"xbuild" ) run_xbuild ;;
 	"release" ) run_release ;;
 
 	#
